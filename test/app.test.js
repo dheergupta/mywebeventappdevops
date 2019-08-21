fac12afbe032af99c6d221f1eb77389adb74b860
Dheeraj Gupta <dheergupta@deloitte.com>
2019-08-21 11:42:41 -0400

    Test updated

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,11 @@
-const app = require('../app');
+const app = require("../app");
 
-const request = require('supertest');
+const request = require("supertest");
 
-describe('GET /', () => {
-  it('should get 200', done => {
+describe("GET /", () => {
+  it("should get 200", done => {
     request(app)
-      .get('/')
+      .get("/")
       .expect(200, done);
-  }),
-    it('should get Hello World', done => {
-      request(app)
-        .get('/')
-        .expect('Hello, world!', done);
-    });
+  });
 });
